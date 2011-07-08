d5e1a15788890ce6e6cc95f74304cdc41f49af8c
Kevin Lau <darealcaffeine@gmail.com>
2011-07-07 21:57:25 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PApp::Application.config.secret_token = 'c50859ac16e1640140322f0ff35848026f5913cce6a182c4123108e84b3a6cc17cd6c24593cb274cc6e71006f15b2a6eb38e0aadd23febb0b940263d8ce6ddec'
